f7ddbd3052f395595f0c1270fb7355402cbe5ca4
gusev-r <gusevrv0o@gmail.com>
2016-11-15 19:00:51 +0300

    add synchronize with browser

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,42 @@ var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var rename = require("gulp-rename");
 
-// Действия по умолчанию
+// synchronize with browser
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+	});
+});
+
+// compile less
 gulp.task('less', function(){
     gulp.src('./less/incLess.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(cleanCSS())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./css'));
+		.pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./app/css'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-// Действия по умолчанию
-gulp.task('default', function(){
-    gulp.run('less');
-
-// Отслеживаем изменения в файлах
-    gulp.watch("./less/*.less", function(event){
+// default watcher
+gulp.task('watch', function(){
+    gulp.watch("./less/*.less", ['browserSync', 'less'], function(event){
         gulp.run('less');
     });
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+
+gulp.task('default', function(callback) {
+  runSequence(['less', 'browserSync'], 'watch',
+    callback
+  )
 });
